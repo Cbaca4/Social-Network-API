0d59b987bdcdc9e57cbf48268247e35d1f4cc22f
Cristian <139021004+Cbaca4@users.noreply.github.com>
2024-01-05 21:26:02 -0600

    added controllers

---
+++ Controllers/thoughtcontrollers.js
@@ -0,0 +1,34 @@
+const { User, Thought } = require("../models");
+
+module.exports = {
+    getThrought(req, res) {
+        Thought.find({})
+        .then((thought) => res.json(thought))
+        .catch((err) => res.status(500).json(err));
+    },
+getSingleThought(req, res) {
+    Thought.findOne({ _id: req.params.thoughtId})
+     .select("-__v")
+     .then((thought) =>
+     !thought
+     ? res.status(404).json({ message: "No thought found with this ID!"})
+     : res.json(thought)
+     )
+     .catch((err) => res.status(500).json(err));
+},
+createThought(req, res) {
+    Thought.create(req.body)
+    .then(({ _id }) => {
+        return User.findOneAndUpdate(
+            { _id: req.body.userId },
+            ( $push: { thoughts: _id } ),
+            { new: true }
+        );
+    })
+    .then((thought) =>
+    !thought
+    ? res.status(404).json({ message: "No user found with this ID!"})
+    : res.json(thought))
+}
+b 
+}
