d967d56e6d5e60037f8bb86750b9962983958ac3
Cristian <139021004+Cbaca4@users.noreply.github.com>
2024-01-08 21:06:47 -0600

    index

--- Controllers/usercontroller.js
+++ Controllers/usercontroller.js
@@ -42,6 +42,79 @@ const userController = {
         }
     },
 
-     
-    }
-}
+    async updateUser(req, res) {
+        try {
+            const user = await User.findOneAndUpdate(
+                {_id: req.params.userId },
+                { $set: req.body },
+                { runValidators: true, new: true }
+            );
+
+            if (!user) {
+                return res.status(404).json({ message: "No user with this id!" });
+            }
+
+            return res.status(200).json(user);
+        } catch (err) {
+            console.log(err);
+            return res.status(500).json(err);
+        }
+    },
+
+    async deleteUser(req, res) {
+        try {
+            const user = await User.findOneAndDelete({ _id: req.params.userId });
+
+            if (!user) {
+                return res.status(404).json({ message: "No user with that ID"});
+            }
+
+            await Thought.deleteMany({ _id: { $in: user.thoughts } });
+            return res.status(200).json({
+                message: "User and associated thoughts and reactions deleted!",
+            });
+        } catch (err) {
+            console.log(err);
+            return res.status(500).json(err);
+        }
+    },
+
+    async addFriend(req, res) {
+        try {
+            const friend = await User.findOneAndUpdate(
+                {_id: req.params.userId },
+                { $addToSet:{ friends: req.params.friendId } },
+                { runValidators: true, new: true }
+            );
+                if (!friend) {
+                    return res.status(404).json({ message: "No user with that ID" });
+                }
+
+                return res.status(200).json(friend);
+        } catch (err) {
+            console.log(err);
+            return res.status(500).json(err);
+        }
+    },
+
+    async deleteFriend(req, res) {
+        try {
+            const friend = await User.findOneAndUpdate(
+                {_id: req.params.userId },
+                { $pull: { friends: req.params.friendId } },
+                { runValidators: true, new: true }
+            );
+
+            if (!friend) {
+                return res.status(404).json({ message: "Check user and friend ID"});
+            }
+
+            return res.status(200).json(friend);
+        } catch (err) {
+            console.log(err);
+            return res.status(500).json(err);
+        }
+    },
+};
+
+module.exports = userController;
